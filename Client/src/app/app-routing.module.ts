9f20f3205f79edfbc37d3646d0236ad24d308cbe
Jose Luis Rendon <jose.rendon@blueoceantech.com.mx>
2019-08-14 19:57:39 -0500

    login

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component: LoginComponent},  
+  {path:'inicio', component: NavbarComponent},  
+  { path:'', pathMatch:'full',redirectTo:'/login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
