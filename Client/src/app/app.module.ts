8e90211625a6ef1ec063aa7c741a221f0ab24f34
JorgeTzun <jorge.tzun.fm@gmail.com>
2019-08-14 20:04:48 -0500

    Maquetado de Busqueda Productos

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ListThingsComponent } from './components/list-things/list-things.component';
+import { ListResultsComponent } from './components/list-results/list-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    ListThingsComponent,
+    ListResultsComponent
   ],
   imports: [
     BrowserModule,
---
+++ Client/src/app/components/list-results/list-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-results',
+  templateUrl: './list-results.component.html',
+  styles: []
+})
+export class ListResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
