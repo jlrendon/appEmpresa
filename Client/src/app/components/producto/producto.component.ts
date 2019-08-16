9937ead696c4c2d2b7fab38d43d5210cd1adaa95
Jose Luis Rendon <jose.rendon@blueoceantech.com.mx>
2019-08-15 21:13:00 -0500

    productos

--- Client/src/app/components/producto/producto.component.ts
+++ Client/src/app/components/producto/producto.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductoService } from 'src/app/service/producto.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-producto',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent implements OnInit {
-
-  constructor() { }
+  productosempresa:any;
+  productos:any;
+  productForm: FormGroup;
+  constructor(private ProductoSvc: ProductoService, private router: Router) {
+    this.productForm = new FormGroup({
+        cantidad: new FormControl('', [Validators.email, Validators.required]),
+        precio: new FormControl('',[Validators.required]),
+        producto: new FormControl('')          
+    })
+  }
 
   ngOnInit() {
+    this.getProducts();
+    this.getAllProducts();
+  }
+
+  getAllProducts(){    
+    this.productosempresa = this.ProductoSvc.getProductosEmpresa();
+  }
+
+  getProducts(){
+    this.productos  =  this.ProductoSvc.getProductos();
+    console.log(this.productos);
+  }
+
+  RegisterProduct(form) {
+    console.log(form.value);
   }
 
 }
